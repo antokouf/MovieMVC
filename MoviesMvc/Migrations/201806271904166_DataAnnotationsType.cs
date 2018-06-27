d79e495b1f6d2cc48d880fe884988eb7a3a7204c
antokouf <antokoufis@gmail.com>
2018-06-27 22:17:58 +0300

    final

---
+++ MoviesMvc/Migrations/201806271904166_DataAnnotationsType.cs
@@ -0,0 +1,16 @@
+namespace MoviesMvc.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataAnnotationsType : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
